f91c2ee79e62b92a7e3a100fa94ec0421d013f6e
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2020-03-19 19:43:23 -0500

    Import Source

---
+++ src/main/java/io/github/garrettsummerfield/ffireworks/EventsListener.java
@@ -0,0 +1,89 @@
+package io.github.garrettsummerfield.ffireworks;
+
+import java.util.Random;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+
+public class EventsListener
+        implements Listener {
+    main configGetter;
+
+    public EventsListener(main main) {
+        this.configGetter = main;
+    }
+
+
+    @EventHandler
+    public void onSnowBallThrow(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getAction() == Action.RIGHT_CLICK_AIR)
+        {
+            if (p.hasPermission("flashingfireworks.throw"))
+            {
+                if (p.getItemInHand().getType() == Material.SNOWBALL)
+                {
+                    if (p.getItemInHand().getItemMeta().hasDisplayName())
+                    {
+                        if (p.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("§bFlashing Firework Snowball")) {
+
+                            Projectile projectile = p.launchProjectile(Snowball.class);
+                            Throw(0, (Entity)projectile);
+                            e.setCancelled(true);
+
+                            if (p.getGameMode() != GameMode.CREATIVE) {
+
+                                ItemStack is = new ItemStack(Material.SNOWBALL, 1);
+                                ItemMeta im = is.getItemMeta();
+                                im.setDisplayName("§bFlashing Firework Snowball");
+                                is.setItemMeta(im);
+
+                                p.getInventory().removeItem(new ItemStack[] { is });
+                                p.updateInventory();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void Throw(int i, final Entity l) {
+        for (int b = 0; b <= 250; b++) {
+
+            i = b;
+
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask((Plugin)this.configGetter, new Runnable()
+            {
+                public void run() {
+                    if (!l.isDead())
+                    {
+                        Random r = new Random();
+                        Color c = Color.fromBGR(r.nextInt(256), r.nextInt(256), r.nextInt(256));
+                        Random r1 = new Random();
+                        Color c1 = Color.fromBGR(r1.nextInt(256), r1.nextInt(256), r1.nextInt(256));
+                        FireworkEffect fireworkEffect = FireworkEffect.builder().flicker(false).trail(true).with(FireworkEffect.Type.BURST).withColor(c).withFade(c1).build();
+
+                    }
+
+                }
+            });
+        }
+    }
+}
+
---
+++ src/main/java/io/github/garrettsummerfield/ffireworks/main.java
@@ -0,0 +1,109 @@
+package io.github.garrettsummerfield.ffireworks;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.EventListener;
+
+
+public class main extends JavaPlugin implements Listener {
+    public void onEnable() {
+        getLogger().info("FlashingFireworks was enabled by console.");
+
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
+        pm.registerEvents(new EventsListener(this), this);
+    }
+
+
+    public void onDisable() {
+        getLogger().info("FlashingFireworks was disabled by console.");
+    }
+
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("flashingfireworks") || cmd.getName().equalsIgnoreCase("ff")) {
+
+            if (!(sender instanceof Player)) {
+
+                sender.sendMessage(ChatColor.RED + "Only players can use FlashingFireworks commands!");
+                return true;
+            }
+
+            Player player = (Player) sender;
+
+            ArrayList<String> arglist = new ArrayList<>();
+            String[] arrayOfString;
+            int j = (arrayOfString = args).length;
+            for (int i = 0; i < j; i++) {
+
+                String s = arrayOfString[i];
+
+                arglist.add(s);
+            }
+
+            if (args == null || arglist.isEmpty()) {
+
+                sendHelp(player);
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("give")) {
+
+                if (player.hasPermission("flashingfireworks.give")) {
+
+                    Player ptgt = player;
+                    if (args.length >= 2 && args != null) {
+
+                        if (Bukkit.getPlayer(args[1]) == null) {
+
+                            player.sendMessage("§cSorry, but this player is not online at the moment.");
+                            return true;
+                        }
+
+                        if (Bukkit.getPlayer(args[1]).isOnline()) {
+                            ptgt = Bukkit.getPlayer(args[1]);
+                        }
+                    }
+
+                    ItemStack is = new ItemStack(Material.SNOWBALL, 64);
+                    ItemMeta im = is.getItemMeta();
+                    im.setDisplayName("§bFlashing Firework Snowball");
+                    is.setItemMeta(im);
+
+                    player.getInventory().addItem(is);
+                    player.sendMessage("§7Given " + ptgt.getName() + " §ax64 Flashing Firework Snowballs §7for them to use.");
+                    ptgt.sendMessage("§7Here is §ax64 Flashing Firework Snowballs §7for you to use.");
+                    return true;
+                }
+
+                player.sendMessage("§cYou don't have enough permission to do that.");
+                return true;
+            }
+
+            sendHelp(player);
+            return true;
+        }
+
+        return true;
+    }
+
+
+    public void sendHelp(Player player) {
+        String version = Bukkit.getServer().getPluginManager().getPlugin("FlashingFireworks").getDescription().getVersion();
+        player.sendMessage("§b§lFlashingFireworks v" + version + " §aby Wulf Games.");
+        player.sendMessage("§3/ff - §7View command help page.");
+        player.sendMessage("§3/ff give [player]- §7Gives you or [player] a stack of flashing firework snowballs.");
+    }
+}
